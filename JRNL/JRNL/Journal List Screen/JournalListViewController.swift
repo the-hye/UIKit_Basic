14a4e794b8b2500fd2c1c6adb1385cfc6073f649
the-hye <ganzi410@gmail.com>
2024-05-10 13:28:23 +0900

    Sample Data Binding

---
+++ JRNL/JRNL/Journal List Screen/JournalListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  JRNL
+//
+//  Created by JIHYE SEOK on 5/7/24.
+//
+
+import UIKit
+
+class JournalListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    var sampleJournalEntryData = SampleJournalEntryData()
+    
+    // MARK: - Properties
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        sampleJournalEntryData.createSampleJournalEntryData()
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        sampleJournalEntryData.journalEntries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let journalCell = tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath) as! JournalListTableViewCell
+        let journalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
+        journalCell.photoImageView.image = journalEntry.photo
+        journalCell.dateLabel.text = journalEntry.date.formatted(.dateTime.year().month().day())
+        journalCell.titleLabel.text = journalEntry.entryTitle
+        
+        return journalCell
+    }
+    
+    // MARK: - Methods
+    @IBAction func unwindNewEntryCancel(segue: UIStoryboardSegue) { }
+}
+
