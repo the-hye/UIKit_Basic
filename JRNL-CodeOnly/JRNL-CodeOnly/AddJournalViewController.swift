1fe4f19053cc71b3161a19b0e5ad8ecc99beeedc
the-hye <ganzi410@gmail.com>
2024-05-13 10:49:25 +0900

    Add Plus Button in JournalListView

---
+++ JRNL-CodeOnly/JRNL-CodeOnly/AddJournalViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddJournalViewController.swift
+//  JRNL-CodeOnly
+//
+//  Created by JIHYE SEOK on 5/13/24.
+//
+
+import UIKit
+
+class AddJournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "New Entry"
+        view.backgroundColor = .white
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- JRNL-CodeOnly/JRNL-CodeOnly/JournalListViewController.swift
+++ JRNL-CodeOnly/JRNL-CodeOnly/JournalListViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class JournalListViewController: UIViewController {
 
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let global = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: global.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: global.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: global.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: global.bottomAnchor),
+        ])
+        
+        navigationItem.title = "Journal"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addJournal))
+    }
+    
+    @objc private func addJournal() {
+        let addJournalViewController = AddJournalViewController()
+        let navigationController = UINavigationController(rootViewController: addJournalViewController)
+        present(navigationController, animated: true)
     }
 
 
