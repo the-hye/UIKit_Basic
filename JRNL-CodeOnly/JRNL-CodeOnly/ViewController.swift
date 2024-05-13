ac0bed2e5fc0b17a406c483abce4f8d897b13331
the-hye <ganzi410@gmail.com>
2024-05-13 09:10:59 +0900

    Initialize JRNL-CodeOnly

---
+++ JRNL-CodeOnly/JRNL-CodeOnly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JRNL-CodeOnly
+//
+//  Created by JIHYE SEOK on 5/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
