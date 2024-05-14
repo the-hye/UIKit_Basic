336e905bbd99b38ac49ce52a0684e019ab3acd1a
the-hye <ganzi410@gmail.com>
2024-05-14 13:05:03 +0900

    JRNL CodeOnly

--- JRNL-CodeOnly/JRNL-CodeOnly/AddJournalViewController.swift
+++ JRNL-CodeOnly/JRNL-CodeOnly/AddJournalViewController.swift
@@ -7,8 +7,69 @@
 
 import UIKit
 
-class AddJournalViewController: UIViewController {
+protocol AddJournalControllerDelegate: NSObject {
+    func saveJournalEntry(_ journalEntry: JournalEntry)
+}
 
+class AddJournalViewController: UIViewController {
+    
+    weak var delegate: AddJournalControllerDelegate?
+    
+    private lazy var mainContainer: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 40
+        
+        return stackView
+    }()
+    
+    private lazy var ratingView: UIStackView = {
+        let stackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 252, height: 44))
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .yellow
+        
+        return stackView
+    }()
+    
+    private lazy var toggleView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        
+        let switchComponent = UISwitch()
+        let labelComponent = UILabel()
+        labelComponent.text = "Switch Label"
+        stackView.addArrangedSubview(switchComponent)
+        stackView.addArrangedSubview(labelComponent)
+        
+        return stackView
+    }()
+    
+    private lazy var titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Journal Title"
+        
+        return textField
+    }()
+    
+    private lazy var bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Journal Body"
+        
+        return textView
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "tortoise")
+        
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,25 +78,66 @@ class AddJournalViewController: UIViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(save))
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        
+        mainContainer.addArrangedSubview(ratingView)
+        mainContainer.addArrangedSubview(toggleView)
+        mainContainer.addArrangedSubview(titleTextField)
+        mainContainer.addArrangedSubview(bodyTextView)
+        mainContainer.addArrangedSubview(imageView)
+        
+        view.addSubview(mainContainer)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        mainContainer.translatesAutoresizingMaskIntoConstraints = false
+        ratingView.translatesAutoresizingMaskIntoConstraints = false
+        toggleView.translatesAutoresizingMaskIntoConstraints = false
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+        bodyTextView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainContainer.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
+            mainContainer.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
+            mainContainer.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
+            
+            ratingView.widthAnchor.constraint(equalToConstant: 252),
+            ratingView.heightAnchor.constraint(equalToConstant: 44),
+            
+            titleTextField.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
+            titleTextField.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
+            
+            bodyTextView.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
+            bodyTextView.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
+            bodyTextView.heightAnchor.constraint(equalToConstant: 128),
+            
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
     }
     
     @objc func save() {
-        
+        guard let title = titleTextField.text, !title.isEmpty,
+              let body = bodyTextView.text, !body.isEmpty else {
+            return
+        }
+        let journalEntry = JournalEntry(rating: 3, title: title, body: body, photo: UIImage(systemName: "face.smiling"))!
+        delegate?.saveJournalEntry(journalEntry)
+        dismiss(animated: true)
     }
     
     @objc func cancel() {
         dismiss(animated: true)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- JRNL-CodeOnly/JRNL-CodeOnly/JournalListScreen/JournalListViewController.swift
+++ JRNL-CodeOnly/JRNL-CodeOnly/JournalListScreen/JournalListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class JournalListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class JournalListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AddJournalControllerDelegate {
     
     lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -15,8 +15,11 @@ class JournalListViewController: UIViewController, UITableViewDataSource, UITabl
         return tableView
     }()
     
+    var sampleJournalEntryData = SampleJournalEntryData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        sampleJournalEntryData.createSampleJournalEntryData()
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -41,16 +44,21 @@ class JournalListViewController: UIViewController, UITableViewDataSource, UITabl
     
     // MARK: - UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        sampleJournalEntryData.journalEntries.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath) as! JournalListTableViewCell
+        let journalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
+        cell.configureCell(journalEntry: journalEntry)
+        
+        return cell
     }
     
     // MARK: - UITableViewDelegate
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let journalDetailViewController = JournalDetailViewController()
+        let journalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
+        let journalDetailViewController = JournalDetailViewController(journalEntry: journalEntry)
         show(journalDetailViewController, sender: self)
     }
     
@@ -62,9 +70,15 @@ class JournalListViewController: UIViewController, UITableViewDataSource, UITabl
     @objc private func addJournal() {
         let addJournalViewController = AddJournalViewController()
         let navigationController = UINavigationController(rootViewController: addJournalViewController)
+        addJournalViewController.delegate = self
         present(navigationController, animated: true)
     }
-
-
+    
+    public func saveJournalEntry(_ journalEntry: JournalEntry) {
+        sampleJournalEntryData.journalEntries.append(journalEntry)
+        tableView.reloadData()
+    }
+    
+    
 }
 
