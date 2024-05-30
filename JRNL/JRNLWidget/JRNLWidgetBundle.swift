8e11975ef95679aee814658cffbc6f292686ac91
the-hye <ganzi410@gmail.com>
2024-05-30 13:06:36 +0900

    Add Widget

---
+++ JRNL/JRNLWidget/JRNLWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  JRNLWidgetBundle.swift
+//  JRNLWidget
+//
+//  Created by JIHYE SEOK on 5/30/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct JRNLWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        JRNLWidget()
+    }
+}
